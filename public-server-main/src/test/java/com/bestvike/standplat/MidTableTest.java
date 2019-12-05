3dead1d01d09b9aacc64ce7f934f42c91f421f80
yinxunyang <yinxunyang>
2019-12-05 17:10:14 +0800

    添加elastic包

--- public-server-main/src/test/java/com/bestvike/standplat/MidTableTest.java
+++ public-server-main/src/test/java/com/bestvike/standplat/MidTableTest.java
@@ -81,6 +81,30 @@ public class MidTableTest extends BaseTest {
 		IndexResponse response = client.prepareIndex(index, type, id).setSource(doc).get();
 		System.out.println("=============" + response.status());
 	}
+	@Test
+	public void esAddTestaa() throws Exception {
+		String index = "house_index";
+		String type = "house_type";
+		// 唯一编号
+		String id = "11";
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+		Settings settings = Settings.builder().put("cluster.name", "docker-cluster").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.237.131"), 9300));
+		XContentBuilder doc = XContentFactory.jsonBuilder()
+				.startObject()
+				.field("buycertnos", "370102199805210012")
+				.field("regionname", "保利华庭")
+				.field("bldname", "1幢")
+				.field("cellname", "2单元")
+				.field("floorname", "5楼")
+				.field("roomno", "301室")
+				.field("buynames", "李五")
+				.field("houseAddress", "奥体中路88号")
+				.endObject();
+		IndexResponse response = client.prepareIndex(index, type, id).setSource(doc).get();
+		System.out.println("=============" + response.status());
+	}
 
 
 }
