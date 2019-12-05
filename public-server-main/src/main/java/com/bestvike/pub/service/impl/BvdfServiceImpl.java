fb7c35574f6d2f194bffae2855043a9ab5fdd986
yinxunyang <yinxunyang>
2019-12-05 17:37:25 +0800

    添加elastic包

--- public-server-main/src/main/java/com/bestvike/pub/service/impl/BvdfServiceImpl.java
+++ public-server-main/src/main/java/com/bestvike/pub/service/impl/BvdfServiceImpl.java
@@ -4,17 +4,33 @@ import com.bestvike.portal.service.BaseService;
 import com.bestvike.pub.dao.BvdfHouseDao;
 import com.bestvike.pub.param.BvdfHouseParam;
 import com.bestvike.pub.service.BvdfService;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BvdfServiceImpl extends BaseService implements BvdfService {
 	@Autowired
 	private BvdfHouseDao bvdfHouseDao;
+	final TransportClient client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", "docker-cluster").build())
+			.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.237.131"), 9300));
 
+	public BvdfServiceImpl() throws UnknownHostException {
+	}
 	/**
 	 * @Author: yinxunyang
 	 * @Description: 定时5分钟一次, 开局自动执行
@@ -26,7 +42,35 @@ public class BvdfServiceImpl extends BaseService implements BvdfService {
 	@Scheduled(fixedRate = 1000 * 60 * 5)
 	public List<BvdfHouseParam> queryBvdfHouseInfo() {
 		List<BvdfHouseParam> bvdfHouseParamList = bvdfHouseDao.queryBvdfHouseInfo();
+
 		// todo 往es数据库迁移数据和其他用户传数据
+		String index = "house_index";
+		String type = "house_type";
+		bvdfHouseParamList.stream().forEach(bvdfHouseParam -> {
+			// 唯一编号 todo 改成序列
+			String id = UUID.randomUUID().toString();
+			//System.setProperty("es.set.netty.runtime.available.processors", "false");
+			XContentBuilder doc = null;
+			try {
+				doc = XContentFactory.jsonBuilder()
+						.startObject()
+						.field("buycertnos", bvdfHouseParam.getBuycertnos())
+						.field("regionname", bvdfHouseParam.getRegionname())
+						.field("bldname", bvdfHouseParam.getBldname())
+						.field("cellname", bvdfHouseParam.getCellname())
+						.field("floorname", bvdfHouseParam.getFloorname())
+						.field("roomno", bvdfHouseParam.getRoomno())
+						.field("buynames", bvdfHouseParam.getBuynames())
+						.field("houseAddress", bvdfHouseParam.getHouseAddress())
+						.endObject();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			IndexResponse response = client.prepareIndex(index, type, id).setSource(doc).get();
+			System.out.println("=============" + response.status());
+		});
+
+
 		int i= 0;
 		return bvdfHouseParamList;
 	}
