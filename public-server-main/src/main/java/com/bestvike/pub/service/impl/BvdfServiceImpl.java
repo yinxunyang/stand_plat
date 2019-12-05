da4606f4481d5c4a530dc32d39cd71612a8f7a1c
yinxunyang <yinxunyang>
2019-12-05 13:43:31 +0800

    asasas

--- public-server-main/src/main/java/com/bestvike/pub/service/impl/BvdfServiceImpl.java
+++ public-server-main/src/main/java/com/bestvike/pub/service/impl/BvdfServiceImpl.java
@@ -5,6 +5,7 @@ import com.bestvike.pub.dao.BvdfHouseDao;
 import com.bestvike.pub.param.BvdfHouseParam;
 import com.bestvike.pub.service.BvdfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,15 @@ public class BvdfServiceImpl extends BaseService implements BvdfService {
 	@Autowired
 	private BvdfHouseDao bvdfHouseDao;
 
+	/**
+	 * @Author: yinxunyang
+	 * @Description: 定时5分钟一次, 开局自动执行
+	 * @Date: 2019/12/5 13:41
+	 * @param:
+	 * @return:
+	 */
 	@Override
+	@Scheduled(fixedRate = 1000 * 60 * 5)
 	public List<BvdfHouseParam> queryBvdfHouseInfo() {
 		List<BvdfHouseParam> bvdfHouseParamList = bvdfHouseDao.queryBvdfHouseInfo();
 		// todo 往es数据库迁移数据和其他用户传数据
