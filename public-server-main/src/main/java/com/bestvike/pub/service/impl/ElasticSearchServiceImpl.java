70e0a8897c217b14e7683fb70407ea3a2920e15a
yinxunyang <yinxunyang>
2019-12-09 10:16:47 +0800

    更改日志

--- public-server-main/src/main/java/com/bestvike/pub/service/impl/ElasticSearchServiceImpl.java
+++ public-server-main/src/main/java/com/bestvike/pub/service/impl/ElasticSearchServiceImpl.java
@@ -42,12 +42,15 @@ public class ElasticSearchServiceImpl implements ElasticSearchService {
 				.field("houseAddress", bvdfHouseParam.getHouseAddress())
 				.endObject();
 		try (TransportClient client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", "docker-cluster").build())
-				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.237.131"), 9300))) {
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.237.132"), 9300))) {
 			IndexResponse response = client.prepareIndex(index, type, id).setSource(doc).get();
-			if ("Created".equals(response.status())) {
-				log.info("往ElasticSearch迁移成功一条数据" + response.status());
+			String esStatus = response.status().toString();
+			if ("CREATED".equals(esStatus)) {
+				log.info("往ElasticSearch新增成功一条数据");
+			} else if ("OK".equals(esStatus)) {
+				log.info("往ElasticSearch更新成功一条数据");
 			} else {
-				throw new Exception("往ElasticSearch迁移失败");
+				throw new Exception("往ElasticSearch迁移失败response:" + response);
 			}
 
 		}
