e6bb97bb0fd8cbfa7d5bb989a1e6b9cee5bd91a5
yinxunyang <yinxunyang>
2019-12-19 19:23:31 +0800

    将bvrfis公司信息跟es中的匹配

--- public-server-main/src/main/java/com/bestvike/bvrfis/service/impl/BvrfisServiceImpl.java
+++ public-server-main/src/main/java/com/bestvike/bvrfis/service/impl/BvrfisServiceImpl.java
@@ -114,40 +114,49 @@ public class BvrfisServiceImpl implements BvrfisService {
 			log.info("bvrfis没有需要跟elasticsearch匹配的开发企业数据");
 			return;
 		}
-		// 跟es匹配和跟开发企业建立关联关系
-		matchEsAndRelationCorp(bvrfisCorpInfoParamList);
+		try (TransportClient client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", esClusterName).build())
+				.addTransportAddress(new TransportAddress(InetAddress.getByName(esIP), Integer.parseInt(esPort)))) {
+			// 开发企业根据组织机构代码完全匹配
+			uniqueMatchCorp(bvrfisCorpInfoParamList, client);
+			// TODO 开发企业根据单位名称完全匹配
+
+			// TODO 开发企业根据单位名称疑似匹配
+		} catch (UnknownHostException e) {
+			log.error("创建elasticsearch客户端连接失败" + e);
+			throw new MsgException(ReturnCode.sdp_sys_error, "创建elasticsearch客户端连接失败");
+		}
 	}
 
 	/**
 	 * @Author: yinxunyang
-	 * @Description: 跟es匹配和跟开发企业建立关联关系
-	 * @Date: 2019/12/19 16:47
+	 * @Description: 开发企业根据组织机构代码完全匹配
+	 * @Date: 2019/12/19 19:15
 	 * @param:
 	 * @return:
 	 */
-	private void matchEsAndRelationCorp(List<BvrfisCorpInfoParam> bvrfisCorpInfoParamList) {
-		try (TransportClient client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", esClusterName).build())
-				.addTransportAddress(new TransportAddress(InetAddress.getByName(esIP), Integer.parseInt(esPort)))) {
-			// 完全匹配开发企业信息
-			ClassPathResource classPathResource = new ClassPathResource("elasticSearch/uniqueMatchCorpQuery.json");
-			// 遍历开发企业信息和elasticsearch
-			bvrfisCorpInfoParamList.forEach(bvrfisCorpInfoParam -> {
-				try {
-					InputStream inputStream = classPathResource.getInputStream();
-					BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-					StringBuffer sb = new StringBuffer();
-					String line;
-					while ((line = br.readLine()) != null) {
-						sb.append(line);
-					}
-					String corpQueryParam = sb.toString().replace("certificateNoValue", bvrfisCorpInfoParam.getLicenseNo());
-					log.info(corpQueryParam);
-					WrapperQueryBuilder wqb = QueryBuilders.wrapperQuery(corpQueryParam);
-					SearchResponse searchResponse = client.prepareSearch(corpindex)
-							.setTypes(corptype).setQuery(wqb).setSize(1).get();
-					SearchHit[] hits = searchResponse.getHits().getHits();
-					// 如果返回唯一一条数据，说明完全匹配
-					if (hits.length == 1) {
+	private void uniqueMatchCorp(List<BvrfisCorpInfoParam> bvrfisCorpInfoParamList, TransportClient client) {
+		// 完全匹配开发企业信息
+		ClassPathResource classPathResource = new ClassPathResource("elasticSearch/uniqueMatchCorpQuery.json");
+		// 遍历开发企业信息和elasticsearch
+		bvrfisCorpInfoParamList.forEach(bvrfisCorpInfoParam -> {
+			try {
+				InputStream inputStream = classPathResource.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+				}
+				String corpQueryParam = sb.toString().replace("certificateNoValue", bvrfisCorpInfoParam.getLicenseNo());
+				log.info(corpQueryParam);
+				WrapperQueryBuilder wqb = QueryBuilders.wrapperQuery(corpQueryParam);
+				SearchResponse searchResponse = client.prepareSearch(corpindex)
+						.setTypes(corptype).setQuery(wqb).setSize(1).get();
+				SearchHit[] hits = searchResponse.getHits().getHits();
+				// 如果返回唯一一条数据，说明完全匹配
+				if (hits.length == 1) {
+					for (SearchHit hit : hits) {
+						String content = hit.getSourceAsString();
 						BmatchAnResultInfo bmatchAnResultInfo = new BmatchAnResultInfo();
 						bmatchAnResultInfo.setMatchid(UtilTool.UUID());
 						bmatchAnResultInfo.setInuser("123");
@@ -157,28 +166,14 @@ public class BvrfisServiceImpl implements BvrfisService {
 						bmatchAnResultInfoList.add(bmatchAnResultInfo);
 						bmatchAnResultDao.insertBmatchAnResult(bmatchAnResultInfoList);
 					}
-					for (SearchHit hit : hits) {
-						String content = hit.getSourceAsString();
-						// 房屋主键
-						log.info(hit.getId());
-						log.info(content);
-					}
-					// todo 1 完全匹配
-					// todo 2 疑似匹配
-
-
-				} catch (MsgException e) {
-					log.error(e + "bvrfisCorpInfoParam参数为：{}", bvrfisCorpInfoParam);
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
-			});
-		} catch (UnknownHostException e) {
-			log.error("创建elasticsearch客户端连接失败" + e);
-			throw new MsgException(ReturnCode.sdp_sys_error, "创建elasticsearch客户端连接失败");
-		}
+			} catch (MsgException e) {
+				log.error(e + "bvrfisCorpInfoParam参数为：{}", bvrfisCorpInfoParam);
+			} catch (IOException e) {
+				log.error(e + "bvrfisCorpInfoParam参数为：{}", bvrfisCorpInfoParam);
+			}
+		});
 	}
-
 	/**
 	 * @Author: yinxunyang
 	 * @Description: 将bvrfis房屋信息跟es中的匹配
