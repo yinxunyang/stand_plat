52f056866c7025e7256c81519b613dbb424c9384
yinxunyang <yinxunyang>
2019-12-23 11:11:39 +0800

    将bvrfis公司信息跟es中的匹配

--- public-server-main/src/main/java/com/bestvike/bvrfis/biz/impl/BvrfisCorpBizImpl.java
+++ public-server-main/src/main/java/com/bestvike/bvrfis/biz/impl/BvrfisCorpBizImpl.java
@@ -1,7 +1,6 @@
 package com.bestvike.bvrfis.biz.impl;
 
 import com.bestvike.bvrfis.biz.BvrfisCorpBiz;
-import com.bestvike.bvrfis.dao.BmatchAnResultDao;
 import com.bestvike.bvrfis.entity.BmatchAnResultInfo;
 import com.bestvike.bvrfis.param.BvrfisCorpInfoParam;
 import com.bestvike.bvrfis.service.BmatchAnResultService;
@@ -71,6 +70,11 @@ public class BvrfisCorpBizImpl implements BvrfisCorpBiz {
 	 */
 	@Value("${esConfig.corptype}")
 	private String corptype;
+	/**
+	 * 疑似匹配的条数
+	 */
+	@Value("${standplatConfig.unCertainSize}")
+	private String unCertainSize;
 	@Autowired
 	private BvrfisCorpService bvrfisCorpService;
 	@Autowired
@@ -99,10 +103,11 @@ public class BvrfisCorpBizImpl implements BvrfisCorpBiz {
 		try (TransportClient client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", esClusterName).build())
 				.addTransportAddress(new TransportAddress(InetAddress.getByName(esIP), Integer.parseInt(esPort)))) {
 			// 开发企业根据组织机构代码完全匹配
-			uniqueMatchCorp(bvrfisCorpInfoParamList, client, httpSession);
+			//uniqueMatchCorp(bvrfisCorpInfoParamList, client, httpSession);
 			// 开发企业根据单位名称完全匹配
-			uniqueMatchCorpByCorpName(bvrfisCorpInfoParamList, client, httpSession);
-			// TODO 开发企业根据单位名称疑似匹配
+			//uniqueMatchCorpByCorpName(bvrfisCorpInfoParamList, client, httpSession);
+			// 开发企业根据单位名称疑似匹配
+			unCertainCorpByCorpName(bvrfisCorpInfoParamList, client, httpSession);
 		} catch (UnknownHostException e) {
 			log.error("创建elasticsearch客户端连接失败" + e);
 			throw new MsgException(ReturnCode.sdp_sys_error, "创建elasticsearch客户端连接失败");
@@ -140,7 +145,7 @@ public class BvrfisCorpBizImpl implements BvrfisCorpBiz {
 				log.info(corpQueryParam);
 				WrapperQueryBuilder wqb = QueryBuilders.wrapperQuery(corpQueryParam);
 				SearchResponse searchResponse = client.prepareSearch(corpindex)
-						.setTypes(corptype).setQuery(wqb).setSize(1).get();
+						.setTypes(corptype).setQuery(wqb).get();
 				SearchHit[] hits = searchResponse.getHits().getHits();
 				// 如果返回唯一一条数据，说明完全匹配
 				if (hits.length == 1) {
@@ -262,5 +267,75 @@ public class BvrfisCorpBizImpl implements BvrfisCorpBiz {
 			}
 		});
 		bvrfisCorpInfoParamList.removeAll(paramListForDelByCorpName);
+	}
+
+	/**
+	 * @Author: yinxunyang
+	 * @Description: 开发企业根据单位名称疑似匹配
+	 * @Date: 2019/12/19 19:15
+	 * @param:
+	 * @return:
+	 */
+	private void unCertainCorpByCorpName(List<BvrfisCorpInfoParam> bvrfisCorpInfoParamList, TransportClient client, HttpSession httpSession) {
+		// 完全匹配开发企业信息
+		ClassPathResource classPathResource = new ClassPathResource("elasticSearch/unCertainbyCorpName.json");
+		// 遍历开发企业信息和elasticsearch
+		bvrfisCorpInfoParamList.forEach(bvrfisCorpInfoParam -> {
+			try {
+				InputStream inputStream = classPathResource.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+				}
+				String corpQueryParam = sb.toString().replace("corpNameValue", bvrfisCorpInfoParam.getCorpName());
+				log.info(corpQueryParam);
+				WrapperQueryBuilder wqb = QueryBuilders.wrapperQuery(corpQueryParam);
+				SearchResponse searchResponse = client.prepareSearch(corpindex)
+						.setTypes(corptype).setSize(Integer.parseInt(unCertainSize)).setQuery(wqb).get();
+				SearchHit[] hits = searchResponse.getHits().getHits();
+				// 如果返回唯一一条数据，说明完全匹配
+				for (SearchHit hit : hits) {
+					// 返回内容
+					String bvdfCorpJson = hit.getSourceAsString();
+					BvdfCorpParam bvdfCorpParam = (BvdfCorpParam) UtilTool.jsonToObj(bvdfCorpJson, BvdfCorpParam.class);
+					BmatchAnResultInfo bmatchAnResultInfo = new BmatchAnResultInfo();
+					bmatchAnResultInfo.setMatchid(UtilTool.UUID());
+					// todo 待确定
+					bmatchAnResultInfo.setLogid(null);
+					// 维修资金数据ID
+					bmatchAnResultInfo.setWxbusiid(bvrfisCorpInfoParam.getCorpNo());
+					bmatchAnResultInfo.setCenterid(bvdfCorpParam.getDataCenterId());
+					bmatchAnResultInfo.setWqbusiid(bvdfCorpParam.getCorpId());
+					String score = Float.toString(hit.getScore());
+					// 该条数据匹配率
+					bmatchAnResultInfo.setPercent(new BigDecimal(score));
+					// 匹配情况分析
+					bmatchAnResultInfo.setResult("匹配度低");
+					// 匹配状态 匹配成功
+					bmatchAnResultInfo.setRelstate(RelStateEnum.MATCH_SUCCESS.getCode());
+					// 匹配情况说明
+					bmatchAnResultInfo.setDescribe(null);
+					// 备注
+					bmatchAnResultInfo.setRemark("开发企业根据单位名称疑似匹配");
+					// 单位信息表
+					bmatchAnResultInfo.setMatchtype(MatchTypeEnum.DEVELOP.getCode());
+					// todo 创建人 待确定
+					bmatchAnResultInfo.setInuser("无");
+					//bmatchAnResultInfo.setInuser(httpSession.getAttribute(GCC.SESSION_KEY_USERNAME).toString());
+					bmatchAnResultInfo.setIndate(UtilTool.nowTime());
+					// 修改人
+					bmatchAnResultInfo.setEdituser(null);
+					bmatchAnResultInfo.setEditdate(null);
+					bmatchAnResultInfo.setVersion(new BigDecimal(bvdfCorpParam.getVersionnumber()));
+					bmatchAnResultService.insertBmatchAnResult(bmatchAnResultInfo);
+				}
+			} catch (MsgException e) {
+				log.error(e + "bvrfisCorpInfoParam参数为：{}", bvrfisCorpInfoParam);
+			} catch (IOException e) {
+				log.error(e + "bvrfisCorpInfoParam参数为：{}", bvrfisCorpInfoParam);
+			}
+		});
 	}
 }
